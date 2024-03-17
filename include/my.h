e1f7e0396e7a75e3bfcfdcb34de36f7c78c8ef23
EmmaFrx <emma.farineaux@epitech.eu>
2024-03-17 14:49:00 +0100

    [feat] -->  add the .h with lib

---
+++ include/my.h
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2024
+** my .h
+** File description:
+** header for amazed
+*/
+
+#pragma once
+    #define MYSH_H
+    #include <unistd.h>
+    #include <stdlib.h>
+    #include <stdio.h>
