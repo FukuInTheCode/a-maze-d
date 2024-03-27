54f2953975acb65e6aa3be498c01606f81703641
Tom C <tom.carlier@epitech.eu>
2024-03-27 15:19:29 +0100

    [refactor] -->  add the functions prototypes into the include

--- include/my.h
+++ include/my.h
@@ -10,5 +10,18 @@
     #include <unistd.h>
     #include <stdlib.h>
     #include <stdio.h>
+    #include <stdbool.h>
+
+typedef struct robot_s {
+    int id;
+    bool status;
+    struct robot_s *next;
+} robot_t;
+
+robot_t *robot_create(void);
+int robot_destroy(robot_t *);
+int robot_set_id(robot_t *, int);
+int robot_set_status(robot_t *, bool);
+int robot_set_next(robot_t *, robot_t *);
 
 
