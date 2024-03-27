b46c01f8e5ff9affc4d43e87489c8d2e5c51bccf
Tom C <tom.carlier@epitech.eu>
2024-03-27 15:51:47 +0100

    [refactor] -->  add the word array functions

--- include/my.h
+++ include/my.h
@@ -27,6 +27,8 @@ int robot_set_next(robot_t *, robot_t *);
 
 void *my_memset(char *, char, size_t);
 char *my_strtok(char *, char const *);
-
+char **my_str_to_word_array(char const *, char const *);
+int my_free_word_array(char **);
+size_t my_len_word_array(char **);
 
 
