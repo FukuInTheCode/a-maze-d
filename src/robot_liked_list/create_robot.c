2ace5ac48868d1f1b4e40abd3c3e8060af3e885b
EmmaFrx <emma.farineaux@epitech.eu>
2024-03-29 11:10:56 +0100

    [refactor] -->  call the function display_nbr_robots

--- src/robot_liked_list/create_robot.c
+++ src/robot_liked_list/create_robot.c
@@ -17,8 +17,8 @@ int create_robot(char **lines)
             return 84;
     }
     nbr = my_getnbr(lines[0]);
-    write(1, "#number_of_robots\n", 18);
-    my_put_nbr(nbr);
-    write(1, "\n", 1);
+    if (nbr == 0)
+        return 84;
+    display_nbr_robots(nbr);
     return 0;
 }
