f3d96fa91cc010fb3b9d81ecdf92b576798c481d
Tom C <tom.carlier@epitech.eu>
2024-04-08 23:23:39 +0200

    [fix] --> add the id incremention for each additions :)

--- src/robot/robot_add.c
+++ src/robot/robot_add.c
@@ -10,6 +10,7 @@
 int robot_add(robot_t *robot, robot_t *new)
 {
     for (; robot->next; robot = robot->next);
+    robot_set_id(new, robot->id + 1);
     robot_set_next(robot, new);
     return 0;
 }
