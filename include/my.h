904cdfdb7a7a75a741fba7e87d5006ee3392c9e6
Tom C <tom.carlier@epitech.eu>
2024-04-09 11:51:26 +0200

    [feat] -->  move the robots

--- include/my.h
+++ include/my.h
@@ -57,6 +57,7 @@ typedef struct robot_s {
     int id;
     bool status;
     struct robot_s *next;
+    room_t *room;
 } robot_t;
 
 robot_t *robot_create(void);
@@ -87,3 +88,4 @@ int start_pathfinding(room_t *);
 
 int create_robot(char **);
 int set_linked_list_robot(int, robot_t *);
+int move_robots(robot_t *, room_t *);
