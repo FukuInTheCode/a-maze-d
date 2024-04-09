3c1a8a48b49c9bf4100ba30c9ff562736ba98f49
EmmaFrx <emma.farineaux@epitech.eu>
2024-04-09 11:53:49 +0200

    [feat] -->  the programm pass if we have tunnel

--- src/room_linked_list/create_room.c
+++ src/room_linked_list/create_room.c
@@ -5,4 +5,28 @@
 ** create_room.c
 */
 
-"my.h"
+#include "my.h"
+
+int get_if_no_room(char **lines, int i)
+{
+    for (int j = 0; lines[i][j] != '\0'; j++) {
+        if (lines[i][j] == '-' || lines[i][j] == '#')
+            return 1;
+    }
+    return 0;
+}
+
+int create_room(char **lines)
+{
+    char **room = NULL;
+    room_t *head = room_create();
+
+    for (int i = 1; lines[i] != NULL; i++) {
+        if (get_if_no_room(lines, i) == 1)
+            continue;
+        room = my_str_to_word_array(lines[i], " ");
+        if (set_linked_list_room(room, head) == 84)
+            return 84;
+    }
+    return 0;
+}
