8382b7cb19dbbffb70ed47fff12985021ac726fb
EmmaFrx <emma.farineaux@epitech.eu>
2024-04-10 12:28:04 +0200

    [feat] -->  add error handling and set the position of the room inside the linked list

--- src/room_linked_list/set_linked_list_room.c
+++ src/room_linked_list/set_linked_list_room.c
@@ -7,23 +7,38 @@
 
 #include "my.h"
 
+bool is_int(char *line)
+{
+    for (int i = 0; line[i] != '\0'; i++) {
+        if (!(line[i] >= '0' && line[i] <= '9'))
+            return false;
+    }
+    return true;
+}
+
 int get_error(char **line)
 {
     size_t len_line = my_len_word_array(line);
 
     if (len_line != 3)
         return 84;
-    printf("%s\n", line[0]);
+    if(!is_int(line[1]) || !is_int(line[2]))
+        return 84;
     return 0;
 }
 
-int set_head(room_t **head)
+int set_head(room_t **head, char **line)
 {
-    if (*head == NULL)
-        *head = room_create();
-    else
-        room_add(*head, room_create());
+    room_t *tmp = room_create();
+    int x = my_getnbr(line[1]);
+    int y = my_getnbr(line[2]);
 
+    room_set_name(tmp, line[0]);
+    room_set_pos(tmp, x, y);
+    if (*head == NULL)
+        *head = tmp;
+    else
+        room_add(*head, tmp);
     return 0;
 }
 
@@ -31,6 +46,6 @@ int set_linked_list_room(char **line, room_t **head)
 {
     if (get_error(line) == 84)
         return 84;
-    set_head(head);
+    set_head(head, line);
     return 0;
 }
